1b150e0c1d997e9d9951f5751404b02482565066
JeYeongR <wpdud2003@gmail.com>
2025-05-09 12:44:30 +0900

    chore: initial project setup

---
+++ src/test/java/com/example/threedbe/ThreedBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.threedbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreedBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
