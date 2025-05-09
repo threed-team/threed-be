79a192105c575f9b8c75527dffa09ded33257cdc
JeYeongR <wpdud2003@gmail.com>
2025-05-09 18:52:30 +0900

    feat: 전역 예외 처리 클래스 추가

---
+++ src/main/java/com/example/threedbe/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.example.threedbe.common.exception;
+
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		String exceptionMessage = exception.getAllErrors().stream()
+			.map(DefaultMessageSourceResolvable::getDefaultMessage)
+			.collect(Collectors.joining(" | "));
+
+		return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exceptionMessage);
+	}
+
+	@ExceptionHandler(NoResourceFoundException.class)
+	public ProblemDetail handleNoResourceFoundException(NoResourceFoundException exception) {
+		return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
+	}
+
+	@ExceptionHandler(ThreedException.class)
+	public ProblemDetail handleCustomException(ThreedException exception) {
+		return ProblemDetail.forStatusAndDetail(exception.getHttpStatus(), exception.getMessage());
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ProblemDetail handleException(Exception exception) {
+		return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러");
+	}
+
+}
