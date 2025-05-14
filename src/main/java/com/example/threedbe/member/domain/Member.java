2e89058996bb78c334f2e832128572295f82d630
JeYeong <wpdud2003@gmail.com>
2025-05-14 10:28:59 +0900

    북마크 생성 기능 리팩터링 (#11)
    * refactor: 북마크 생성 api 엔드포인트 수정
    
    * refactor: 북마크 생성 로직 리팩터링
    
    * fix: ThreedBadRequestException 를 ThreedNotFoundException 로 변경

--- src/main/java/com/example/threedbe/member/domain/Member.java
+++ src/main/java/com/example/threedbe/member/domain/Member.java
@@ -1,16 +1,21 @@
 package com.example.threedbe.member.domain;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
+import java.util.List;
 
 import com.example.threedbe.auth.domain.RefreshToken;
+import com.example.threedbe.bookmark.domain.Bookmark;
+import com.example.threedbe.common.exception.ThreedConflictException;
+import com.example.threedbe.post.domain.Post;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -41,23 +46,29 @@ public class Member {
 	@Embedded
 	private RefreshToken refreshToken;
 
+	@OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Bookmark> bookmarks;
+
 	private LocalDateTime deletedAt;
 
 	public boolean isDeleted() {
 		return deletedAt != null;
 	}
 
-	@Override
-	public final boolean equals(Object o) {
-		if (!(o instanceof Member member))
-			return false;
+	public void addBookmark(Post post) {
+		validateDuplicateBookmark(post);
 
-		return Objects.equals(id, member.id);
+		Bookmark bookmark = new Bookmark(this, post);
+		this.bookmarks.add(bookmark);
+		post.getBookmarks().add(bookmark);
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hashCode(id);
+	private void validateDuplicateBookmark(Post post) {
+		boolean isAlreadyBookmarked = this.bookmarks.stream()
+			.anyMatch(bookmark -> bookmark.getPost().equals(post));
+		if (isAlreadyBookmarked) {
+			throw new ThreedConflictException("이미 북마크한 게시물입니다.");
+		}
 	}
 
 }
