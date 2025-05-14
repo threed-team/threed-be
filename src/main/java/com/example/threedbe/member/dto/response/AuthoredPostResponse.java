13d66a6a7b31d705c9fb8f10840abdd9fd836a63
JeYeongR <wpdud2003@gmail.com>
2025-05-14 18:30:34 +0900

    feat: 작성한 포스트 리스트 조회 기능 구현

---
+++ src/main/java/com/example/threedbe/member/dto/response/AuthoredPostResponse.java
@@ -0,0 +1,54 @@
+package com.example.threedbe.member.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.example.threedbe.post.domain.MemberPost;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AuthoredPostResponse(
+
+	@Schema(description = "포스트 아이디", example = "2")
+	long id,
+
+	@Schema(description = "제목", example = "FE News 25년 5월 소식을 전해드립니다!")
+	String title,
+
+	@Schema(description = "썸네일 이미지 주소", example = "https://d2.naver.com/content/images/2023/07/-----------2023-07-06------4-16-49.png")
+	String thumbnailImageUrl,
+
+	@Schema(description = "분야", example = "Frontend")
+	String field,
+
+	@Schema(description = "조회수", example = "0")
+	int viewCount,
+
+	@Schema(description = "소속 회사", example = "네이버")
+	String company,
+
+	@Schema(description = "기술들", example = "[\"REACT\", \"JAVASCRIPT\"]")
+	List<String> skills,
+
+	@Schema(description = "생성일", example = "2025-05-08T20:12:14")
+	LocalDateTime createdAt
+
+) {
+
+	public static AuthoredPostResponse from(MemberPost memberPost) {
+		return new AuthoredPostResponse(
+			memberPost.getId(),
+			memberPost.getTitle(),
+			memberPost.getThumbnailImageUrl(),
+			memberPost.getField().getValue(),
+			memberPost.getViewCount(),
+			null,
+			memberPost.getSkills()
+				.stream()
+				.map(skill -> skill.getSkill().getName())
+				.toList(),
+			memberPost.getCreatedAt()
+		);
+	}
+
+}
