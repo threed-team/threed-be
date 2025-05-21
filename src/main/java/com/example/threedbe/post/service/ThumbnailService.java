ab9355641869b33191715021c1f3dd4722b56a2e
JeYeong <wpdud2003@gmail.com>
2025-05-21 09:53:48 +0900

    NotoSansKR 폰트 프로젝트에 포함하여 기본 폰트 설정 (#44)
    * feat: NotoSansKR 폰트 프로젝트에 포함하여 기본 폰트 설정

--- src/main/java/com/example/threedbe/post/service/ThumbnailService.java
+++ src/main/java/com/example/threedbe/post/service/ThumbnailService.java
@@ -2,8 +2,11 @@ package com.example.threedbe.post.service;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.InputStream;
 
 import org.springframework.stereotype.Service;
+
+import com.example.threedbe.common.exception.ThreedServerErrorException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +18,21 @@ public class ThumbnailService {
 	private static final int THUMBNAIL_HEIGHT = 630;
 	private static final Color BACKGROUND_COLOR = new Color(97, 76, 246);
 	private static final Color TEXT_COLOR = Color.WHITE;
-	private static final Font TITLE_FONT = new Font(Font.MONOSPACED, Font.BOLD, 64);
+	private static final int FONT_SIZE = 64;
+	private static final String FONT_PATH = "/fonts/NotoSansKR-Bold.ttf";
+	private static final Font TITLE_FONT = loadFont();
+
+	private static Font loadFont() {
+		try (InputStream is = ThumbnailService.class.getResourceAsStream(FONT_PATH)) {
+			if (is != null) {
+				return Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(Font.BOLD, FONT_SIZE);
+			}
+		} catch (Exception e) {
+			throw new ThreedServerErrorException("폰트 로드 실패");
+		}
+
+		return new Font(Font.MONOSPACED, Font.BOLD, FONT_SIZE);
+	}
 
 	public BufferedImage createThumbnailImage(String title) {
 		BufferedImage image = new BufferedImage(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT, BufferedImage.TYPE_INT_RGB);
